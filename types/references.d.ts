9cc3547826d1cd07c4a308269f52d0ac293ebfe6
PackagedCat <packagedcat@outlook.com>
2022-05-06 22:59:31 +0300

    chore: fix references

--- types/references.d.ts
+++ types/references.d.ts
@@ -1,4 +1,4 @@
-/// <reference path="../node_modules/@nativescript/types-ios/index.d.ts" />
+/// <reference path="../node_modules/@nativescript/types-ios/lib/ios/ios.d.ts" />
 /// <reference path="../node_modules/@nativescript/types-android/lib/android-26.d.ts" />
 
 /// <reference path="./com.google.android.gms.maps.d.ts" />
